31020ec3441dd102ba5b4946222bd4ae68c860f4
Mathew Harb <30761383+mathewharb@users.noreply.github.com>
2018-10-14 20:36:42 +0000

    Update connection.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,7 +1,7 @@
 <?php
 
    try{
-   $pdo=new PDO('mysql:host=localhost;dbname=pdocms', 'root', 'controlpass123');
+   $pdo=new PDO('mysql:host=localhost;dbname=pdocms', 'root', '123');
 
 }catch(PDOException $e){
 	exit('Error could not connect');
